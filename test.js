af077f2dcc4ff379c276ac2978e5cc00633f964f
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-03 19:50:58 +0100

    Test versioned builtin

--- test.js
+++ test.js
@@ -254,6 +254,16 @@ test('bare specifier with builtin', (t) => {
   t.alike(result, ['builtin:d'])
 })
 
+test('bare specifier with versioned builtin', (t) => {
+  const result = []
+
+  for (const resolution of resolve('d', new URL('file:///a/b/c'), { builtins: ['d@1.2.3'] })) {
+    result.push(resolution.href)
+  }
+
+  t.alike(result, ['builtin:d'])
+})
+
 test('bare specifier with conditional builtin', (t) => {
   let result = []
 
