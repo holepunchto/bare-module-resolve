56e3d59244d738945830e546549efc42956280b5
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-02-07 10:20:33 +0100

    Review

--- index.d.ts
+++ index.d.ts
@@ -1,166 +1,165 @@
 import URL from 'bare-url'
 
-type RecursiveStringObject = {
-  [key: string]: RecursiveStringObject | string
-}
-
-type ResolveModuleStatus = number
-
-type ResolveModuleTarget =
+type ConditionalSpecifier =
   | string
-  | RecursiveStringObject
-  | (string | RecursiveStringObject)[]
+  | ConditionalSpecifier[]
+  | { [condition: string]: ConditionalSpecifier }
 
-type ResolveModuleConditions = string[] | string[][]
+type ImportsMap = { [specifier: string]: ConditionalSpecifier }
 
-interface ModuleResolveOptions {
+type ExportsMap = ImportsMap
+
+type ResolutionsMap = { [href: string]: ImportsMap }
+
+type Builtins = ConditionalSpecifier[]
+
+type Conditions = string[] | Conditions[]
+
+type Engines = { [name: string]: string }
+
+type JSON = string | number | boolean | JSON[] | { [key: string]: JSON }
+
+interface ResolveOptions {
   builtinProtocol?: string
-  builtins?: (RecursiveStringObject | string)[]
-  conditions?: ResolveModuleConditions
-  engines?: RecursiveStringObject
+  builtins?: Builtins
+  conditions?: Conditions
+  engines?: Engines
   extensions?: string[]
-  imports?: RecursiveStringObject
+  imports?: ImportsMap
   matchedConditions?: string[]
-  resolutions?: RecursiveStringObject
+  resolutions?: ResolutionsMap
 }
 
 declare function resolve(
   specifier: string,
   parentURL: URL,
-  opts?: ModuleResolveOptions,
-  readPackage?: (url: URL) => unknown
+  readPackage?: (url: URL) => JSON | null
 ): Iterable<URL>
 
 declare function resolve(
   specifier: string,
   parentURL: URL,
-  readPackage: (url: URL) => unknown
-): Iterable<URL> & AsyncIterable<URL>
+  readPackage: (url: URL) => Promise<JSON | null>
+): AsyncIterable<URL>
+
+declare function resolve(
+  specifier: string,
+  parentURL: URL,
+  opts: ResolveOptions,
+  readPackage?: (url: URL) => JSON | null
+): Iterable<URL>
+
+declare function resolve(
+  specifier: string,
+  parentURL: URL,
+  opts: ResolveOptions,
+  readPackage: (url: URL) => Promise<JSON | null>
+): AsyncIterable<URL>
 
 declare namespace resolve {
   export {
-    type ModuleResolveOptions,
-    type ResolveModuleStatus,
-    type ResolveModuleTarget,
-    type ResolveModuleConditions
+    type ConditionalSpecifier,
+    type ImportsMap,
+    type ExportsMap,
+    type ResolutionsMap,
+    type Builtins,
+    type Conditions,
+    type Engines,
+    type ResolveOptions
   }
 
   export const constants: {
-    UNRESOLVED: ResolveModuleStatus
-    YIELDED: ResolveModuleStatus
-    RESOLVED: ResolveModuleStatus
+    UNRESOLVED: number
+    YIELDED: number
+    RESOLVED: number
   }
+
+  export type Resolver = Generator<
+    { package: URL } | { resolution: URL },
+    number,
+    void | boolean | JSON | null
+  >
 
   export function module(
     specifier: string,
     parentURL: URL,
-    opts?: ModuleResolveOptions
-  ): Iterable<{ resolution: URL }, ResolveModuleStatus>
+    opts?: ResolveOptions
+  ): Resolver
 
   export function url(
     url: string,
     parentURL: URL,
-    opts?: ModuleResolveOptions
-  ): Iterable<{ resolution: URL }, ResolveModuleStatus>
+    opts?: ResolveOptions
+  ): Resolver
 
   export function preresolved(
     specifier: string,
-    resolutions: RecursiveStringObject,
+    resolutions: ResolutionsMap,
     parentURL: URL,
-    opts?: ModuleResolveOptions
-  ): Iterable<{ resolution: URL }, ResolveModuleStatus>
+    opts?: ResolveOptions
+  ): Resolver
 
   export function package(
     packageSpecifier: string,
     parentURL: URL,
-    opts?: ModuleResolveOptions
-  ): Iterable<{ resolution?: URL; package?: URL }, ResolveModuleStatus>
+    opts?: ResolveOptions
+  ): Resolver
 
   export function packageSelf(
     packageName: string,
     packageSubpath: string,
     parentURL: URL,
-    opts?: ModuleResolveOptions
-  ): Iterable<{ resolution?: URL; package?: URL }, ResolveModuleStatus>
+    opts?: ResolveOptions
+  ): Resolver
 
   export function packageExports(
     packageURL: URL,
     subpath: string,
-    packageExports: ResolveModuleTarget,
-    opts?: ModuleResolveOptions
-  ): Iterable<{ resolution?: URL; package?: URL }, ResolveModuleStatus>
+    packageExports: ExportsMap,
+    opts?: ResolveOptions
+  ): Resolver
 
   export function packageImports(
     specifier: string,
     parentURL: URL,
-    opts?: ModuleResolveOptions
-  ): Iterable<{ resolution?: URL; package?: URL }, ResolveModuleStatus>
+    opts?: ResolveOptions
+  ): Resolver
 
   export function packageImportsExports(
     matchKey: string,
-    matchObject: RecursiveStringObject,
+    matchObject: ImportsMap | ExportsMap,
     packageURL: URL,
     isImports: boolean,
-    opts?: ModuleResolveOptions
-  ): Iterable<{ resolution?: URL; package?: URL }, ResolveModuleStatus>
-
-  export function validateEngines(
-    packageURL: URL,
-    packageEngines: Record<string, string>,
-    opts?: ModuleResolveOptions
-  ): void
-
-  export function patternKeyCompare(keyA: string, keyB: string): number
+    opts?: ResolveOptions
+  ): Resolver
 
   export function packageTarget(
     packageURL: URL,
-    target: ResolveModuleTarget,
+    target: ConditionalSpecifier,
     patternMatch: string,
     isImports: boolean,
-    opts?: ModuleResolveOptions
-  ): Iterable<{ resolution?: URL; package?: URL }, ResolveModuleStatus>
+    opts?: ResolveOptions
+  ): Resolver
 
   export function builtinTarget(
     packageSpecifier: string,
     packageVersion: string,
-    target: ResolveModuleTarget,
-    opts?: ModuleResolveOptions
-  ): Iterable<{ resolution: URL }, ResolveModuleStatus>
-
-  export function conditionMatches(
-    target: ResolveModuleTarget,
-    conditions: ResolveModuleConditions,
-    opts?: ModuleResolveOptions
-  ): Iterable<
-    [
-      condition: string,
-      target: ResolveModuleTarget,
-      conditions: ResolveModuleConditions
-    ],
-    ResolveModuleStatus
-  >
-
-  export function lookupPackageScope(
-    url: URL,
-    opts?: ModuleResolveOptions
-  ): Iterable<URL>
+    target: ConditionalSpecifier,
+    opts?: ResolveOptions
+  ): Resolver
 
   export function file(
     filename: string,
     parentURL: URL,
     isIndex: boolean,
-    opts?: ModuleResolveOptions
-  ): Iterable<{ resolution: URL }, ResolveModuleStatus>
+    opts?: ResolveOptions
+  ): Resolver
 
   export function directory(
     dirname: string,
     parentURL: URL,
-    opts?: ModuleResolveOptions
-  ): Iterable<{ resolution?: URL; package?: URL }, ResolveModuleStatus>
-
-  export function isWindowsDriveLetter(input: string): boolean
-
-  export function startsWithWindowsDriveLetter(input: string): boolean
+    opts?: ResolveOptions
+  ): Resolver
 }
 
 export = resolve
